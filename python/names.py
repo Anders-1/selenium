e87a3e2e323da5e5a61ede8bdab1e439f76943d2
Anders <ahpyenson@gmail.com>
2022-06-18 16:14:04 -0400

    Found "Fabulous Falcon" again!

--- python/names.py
+++ python/names.py
@@ -1,10 +1,13 @@
 def save_to_file(text):
 
-    with open('./names3.txt', mode='wt', encoding='utf-8') as myfile:
+    with open('./names4.txt', mode='wt', encoding='utf-8') as myfile:
         myfile.write('\n'.join(text))
         myfile.write('\n')
 
-with open('data3.txt', 'r', encoding='utf-8') as file:
+with open('python\data4.txt', 'r', encoding='utf-8') as file:
     data = file.read().replace(' ', '\n')
     data = data.split()
     save_to_file(data)
+
+# Command (bash) to get final.txt from names.txt:
+# sort names.txt | uniq -c | sort -nr > final.txt
